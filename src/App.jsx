e5c6c23a49101cc4dbb0426bf3697db006048393
Edlawit Tsegaye <edlawitdigital3@gmail.com>
2026-09-11 10:31:43 +0300

    add some style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import './App.css';
+import "./App.css";
 function TodoApp() {
   const [taskText, setTaskText] = useState("");
   const [tasks, setTasks] = useState([]);
